1456a6ba735e00695a5fd0e1330d1a7f72fff91d
Dylan Hurtado López <dylahurtado43@gmail.com>
2022-03-20 22:10:58 +0100

    Multipart avatar update and get  READY!

--- controller/user.js
+++ controller/user.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require("path");
 const User = require('../model/User');
 const bcrypt = require('bcrypt');
 const jwt = require('../service/jwt');
@@ -30,7 +32,6 @@ async function login(req,res){
             throw{msg:"Error en el email o password"};
         }
         const passwordSuccess=await bcrypt.compare(password,user.password);
-        console.log(passwordSuccess)
         if(!passwordSuccess){
             throw{msg:"Error en el email o password"};
         }
@@ -49,8 +50,65 @@ async function protected(req,res){
         res.status(500).send(error);
     }
 }
+
+function uploadAvatar(req,res){
+    const params= req.params;
+    User.findById({_id:params.id},(err,userData)=>{
+        if(err){
+            res.status(500).send({msg:"Error del servidor"});
+        }else{
+            if(!userData){
+                res.status(404).send({msg:"No se ha encontrado usuario"})
+            }else{
+                let user = userData;
+                console.log(req.files)
+                if(req.files){
+                    const filePath=req.files.avatar.path;
+                    let fileSplit=filePath.split(path.delimiter)
+                    if(fileSplit.length==1){//Porque no me pilla la doble barra invertida
+                        fileSplit=filePath.split("\\");
+                    }
+                    let fileName = fileSplit[1];
+                    let extSplit = fileName.split(".");
+                    let fileExt= extSplit[1];
+                    if(fileExt!== "png" && fileExt!=="jpg"){
+                        res.status(400).send({msg:"La extension de la imagen no esta permitida (Only .png or .jpg)"})
+                    }else{
+                        user.avatar = fileName;
+
+                        User.findByIdAndUpdate({_id:params.id},user,(err,userResult)=>{
+                            if(err){
+                                res.status(500).send({msg: "Error servidor"});
+                            }else if(!userResult){
+                                res.status(404).send({msg:"No se ha encontrado el usuario"});
+                            }else{
+                                res.status(200).send({msg:"Avatar actualizado"});
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    });
+}
+
+function getAvatar(req,res){
+    const {avatarName} = req.params;
+    const filePath=`./uploads/${avatarName}`;
+    fs.stat(filePath,(err,stat)=>{
+        if(err){
+            res.status(404).send({msg:"El avatar no existe."});
+        }else{
+            res.sendFile(path.resolve(filePath));
+        }
+    });
+
+
+}
 module.exports = {
     register,
     login,
     protected,
+    uploadAvatar,
+    getAvatar,
 }
