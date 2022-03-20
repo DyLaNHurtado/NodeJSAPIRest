590117d69201ee91a78bbe56cef9f8b92f4b0bcf
Dylan Hurtado López <dylahurtado43@gmail.com>
2022-03-20 11:49:35 +0100

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require("./app");
 
 const user = "mongoadmin";
 const password = "mongopass";
-const serverUrl = "0.0.0.0"|| "localhost";
+const serverUrl = "localhost";
 const serverPort = "27017";
 const dataBaseName = "mongodb";
 const URL_MONGODB ="mongodb://"+user+":"+password+"@"+ serverUrl + ":" + serverPort + "/" + dataBaseName + "?authSource=admin";
